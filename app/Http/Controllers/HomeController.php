7a3e538577bf2a6c456efc8aad9ed818c942829a
mars01253 <mauadtoto001@gmail.com>
2024-01-31 22:09:12 +0100

    laravel installation

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Service;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+
+    public function index()
+    {
+        $data = Category::all();
+        $info = Service::orderBy('created_at' , 'desc')->get();
+        // $info = DB::table('services')->select(*, )
+        return view('home', ['data'=>$data , 'service'=>$info]);
+    }
+    
+    public function store(){
+        $name = request()->name;
+        $description = request()->description;
+        $Category = request()->Category;
+        $user =  request()->id ; 
+        $price =  request()->price ; 
+        $insert = new Service ;
+        $insert->name = $name ; 
+        $insert->description = $description ; 
+        $insert->category_id = $Category ; 
+        $insert->user_id = $user ; 
+        $insert->price = $price ; 
+        $insert->save();
+        $data = Category::all();
+        $info = Service::orderBy('created_at' , 'desc')->get();
+        return view('home', ['data'=>$data , 'service'=>$info]);
+    }
+}
